1af45d65bce9cae9a54b3823d0772e1a6a1e2841
Kicajowyfreestyle <piotr.m.kicki@gmail.com>
2021-01-21 12:49:52 +0100

    REmoved unnecessary parts of momentum and added occlusion example

--- momentum.py
+++ momentum.py
@@ -9,6 +9,7 @@ r = 15
 #img = plt.imread("a1.png")[..., 0]
 #start = (156, 324)
 img = plt.imread("test.png")[..., 0]
+#img = plt.imread("test1.png")[..., 0]
 start = (140, 103)
 #img = plt.imread("0019.png")#[..., 0]
 #img = plt.imread("zgiecie.png")#[..., 0]
@@ -18,7 +19,7 @@ start = (140, 103)
 #plt.show()
 img = skeletonize(img)
 
-#plt.subplot(121)
+plt.subplot(121)
 plt.imshow(img)
 plt.plot(start[0], start[1], 'rx')
 path = [start]
@@ -76,12 +77,12 @@ for i in range(100):
 t2 = time()
 print(path)
 print(t2 - t1)
-path = path[:10] + path[20:]
+#path = path[:10] + path[20:]
 xys = np.stack(path, axis=0)
 t = np.linspace(0., 1., xys.shape[0])
 T = np.linspace(0., 1., 128)
-k = 1
-#k = 10
+#k = 1
+k = 10
 #k = 100
 x_spline = UnivariateSpline(t, xys[:, 0], s=k)
 y_spline = UnivariateSpline(t, xys[:, 1], s=k)
@@ -90,9 +91,11 @@ y = y_spline(T)
 t3 = time()
 print(t3 - t1)
 plt.plot(x, y, 'g')
-#plt.subplot(122)
-#plt.plot(T, x, label="x")
-#plt.plot(T, y, label="y")
-#plt.legend()
+plt.subplot(122)
+plt.plot(T, x, label="x")
+plt.plot(t, xys[:, 0], 'x', label="px")
+plt.plot(T, y, label="y")
+plt.plot(t, xys[:, 1], 'x', label="py")
+plt.legend()
 
 plt.show()
