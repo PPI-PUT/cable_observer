c3cef9fd6b41603b8b4191d0a147797249434d40
pkicki <piotr.m.kicki@gmail.com>
2023-02-14 08:21:45 +0100

    Learning scripts

--- src/cable_observer/utils/__init__.py
+++ src/cable_observer/utils/__init__.py
@@ -1,5 +1,7 @@
-from .image_processing import *
-from .paths_processing import *
-from .path import *
-from .tracking import *
-from .utils import *
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from image_processing import *
+from paths_processing import *
+from path import *
+from tracking import *
+from utils import *
